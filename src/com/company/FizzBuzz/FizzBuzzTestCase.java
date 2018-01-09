24b245aca36306c91b793a56e5543b0db4305f56
Eric.Johnson <eric.johnson@astontech.com>
2018-01-09 10:01:10 -0600

    Re-Do

--- src/com/company/FizzBuzz/FizzBuzzTestCase.java
+++ src/com/company/FizzBuzz/FizzBuzzTestCase.java
@@ -9,57 +9,57 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by ericjohn1 on 10/12/2016.
  */
-//public class FizzBuzzTestCase {
-//    private FizzBuzz fb;
-//
-//    @Before
-//    public void setUp() throws Exception{
-//        fb = new FizzBuzz();
-//    }
-//
-//    @Test
-//    public void canEvaluate(){
-//        fb.evaluate(1);
-//    }
-//
-//    @Test
-//    public void oneReturnsOne (){
-//        assertEquals("1", fb.evaluate(1));
-//
-//    }
-//    @Test
-//    public void twoReturnsTwo(){
-//        assertEquals("2", fb.evaluate(2));
-//    }
-//
-//    @Test
-//    public void threeReturnsFizz(){
-//        assertEquals("Fizz", fb.evaluate(3));
-//
-//    }
-//    // we assume that 4 will work, but it wouldnt be a bad idea to test it
-//
-//    @Test
-//    public void fiveReturnsBuzz(){
-//        assertEquals("Buzz" , fb.evaluate(5));
-//    }
-//
-//    @Test
-//    public void sixReturnsSix(){
-//        assertEquals("Fizz", fb.evaluate(6));
-//    }
-//    @Test
-//    public void tenReturnsBuzz(){
-//        assertEquals("Buzz",fb.evaluate(10));
-//    }
-//
-//    @Test
-//    public void fifteenReturnsFizzBuzz(){
-//        assertEquals("FizzBuzz", fb.evaluate(15));
-//    }
-//
-//    @Test
-//    public void thirtyReturnsFizzBuzz(){
-//        assertEquals("FizzBuzz", fb.evaluate(30));
-//    }
-//}
+public class FizzBuzzTestCase {
+    private FizzBuzz fb;
+
+    @Before
+    public void setUp() throws Exception{
+        fb = new FizzBuzz();
+    }
+
+    @Test
+    public void canEvaluate(){
+        fb.eval(1);
+    }
+
+    @Test
+    public void oneReturnsOne (){
+        assertEquals("1", fb.eval(1));
+
+    }
+    @Test
+    public void twoReturnsTwo(){
+        assertEquals("2", fb.eval(2));
+    }
+
+    @Test
+    public void threeReturnsFizz(){
+        assertEquals("Fizz", fb.eval(3));
+
+    }
+    // we assume that 4 will work, but it wouldnt be a bad idea to test it
+
+    @Test
+    public void fiveReturnsBuzz(){
+        assertEquals("Buzz" , fb.eval(5));
+    }
+
+    @Test
+    public void sixReturnsSix(){
+        assertEquals("Fizz", fb.eval(6));
+    }
+    @Test
+    public void tenReturnsBuzz(){
+        assertEquals("Buzz",fb.eval(10));
+    }
+
+    @Test
+    public void fifteenReturnsFizzBuzz(){
+        assertEquals("FizzBuzz", fb.eval(15));
+    }
+
+    @Test
+    public void thirtyReturnsFizzBuzz(){
+        assertEquals("FizzBuzz", fb.eval(30));
+    }
+}
